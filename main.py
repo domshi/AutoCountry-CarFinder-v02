002ac333a54967d06a7e3470da1e9515819c6879
domshi <90562421+domshi@users.noreply.github.com>
2024-06-28 01:53:52 +0000

    Redo

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 AllowedVehiclesList = ['Ford F-150','Chevrolet Silverado','Tesla Cybertruck','Toyota Tundra','Nissan Titan']
 
-print("********************************")
-print("AutoCountry Vehicle Finder v0.1")
-print("********************************")
-print("Please Enter one of the following numbers below from the following menu:")
-print(" ")
-print('1. PRINT all Authorized Vehicles')
-print('2. Exit')
+
 
 while (True):
+  print("********************************")
+  print("AutoCountry Vehicle Finder v0.1")
+  print("********************************")
+  print("Please Enter one of the following numbers below from the following menu:")
+  print(" ")
+  print('1. PRINT all Authorized Vehicles')
+  print('2. Exit')
   selection = input("Please enter your choice: ")
   if int(selection) == 1:
-    print(AllowedVehiclesList)
+    print('The AutoCountry sales manager has authorized the purchase and selling of the following vehicles:')
+    for vehicles in AllowedVehiclesList:
+      print(vehicles)
     continue
   if int(selection) != 2:
     print("Invalid Choice,Please select a valid choice from the menu")
